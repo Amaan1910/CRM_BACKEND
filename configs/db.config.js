378601db4050138fc1eb65f99d9d0ebed610fc74
Amaan Ahmad <amaanahmad321p@gmail.com>
2023-07-07 19:49:46 +0530

    first commit

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,9 @@
+const username = "walkinlove02";
+const password = "walkinlove";
+
 module.exports = {
-    DB_NAME: "crm_db",
-    DB_URL:  process.env.MONGODB_URI || "mongodb://localhost/crm_db"
-}
+  DB_NAME: "crm_db",
+  DB_URL:
+    process.env.MONGODB_URI ||
+    `mongodb+srv://${username}:${password}@cluster0.3l6vivj.mongodb.net/?retryWrites=true&w=majority`,
+};
