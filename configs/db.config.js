1d338b49e843d7f1ce43be1d55af49ac2720c193
Amaan Ahmad <amaanahmad321p@gmail.com>
2023-07-07 22:52:54 +0530

    url

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,9 +1,6 @@
-const username = "walkinlove02";
-const password = "walkinlove";
-
 module.exports = {
   DB_NAME: "crm_db",
   DB_URL:
     process.env.MONGODB_URI ||
-    `mongodb+srv://${username}:${password}@cluster0.3l6vivj.mongodb.net/?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.USERNAME_1}:${process.env.PASSWORD}@cluster0.3l6vivj.mongodb.net/?retryWrites=true&w=majority`,
 };
